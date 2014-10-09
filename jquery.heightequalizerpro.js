270636de4e23feb358571d7b50592249d9684012
Alexander Bech <github@alexanderbech.com>
2014-10-09 18:37:27 +0200

    Added children feature
    Now you can specify if you want to equalize the height of children, by doing:
    $('ul li').heightEqualizerPro('a');
    
    This will check the height of any <a> element and equalize the same height for all li's a tags.

--- jquery.heightequalizerpro.js
+++ jquery.heightequalizerpro.js
@@ -1,13 +1,13 @@
 /* =========================================================
-*	HeightEqualizerPro.js 2.0
+*	HeightEqualizerPro.js 3.0
 *	Credits: Jacob Eriksen
 *	http://github.com/AlexanderBech/HeightEqualizerPro.js
 * ========================================================== */
 (function($){
     $.fn.heightEqualizerPro=function(){
-        var o={};                       
+    	var subItem=arguments[0]||null,o={};
         this.each(function(){
-            var $t=$(this);$t.height('auto');var k='_'+$t.offset().top;
+            var $t=subItem?$(subItem,$(this)):$(this);$t.height('auto');var k='_'+$t.offset().top;
             if(!o[k])o[k]=[];o[k].push({s:$t,h:$t.outerHeight()}); 
         })
         for(var k in o){
