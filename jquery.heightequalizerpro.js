cfd63af85ea0bfab6f196b7147fc9956813ff179
Alexander Bech <github@alexanderbech.com>
2014-10-10 13:53:03 +0200

    Fixed bug

--- jquery.heightequalizerpro.js
+++ jquery.heightequalizerpro.js
@@ -8,11 +8,11 @@
     	var subItem=arguments[0]||null,o={};
         this.each(function(){
             var $t=subItem?$(subItem,$(this)):$(this);$t.height('auto');var k='_'+$t.offset().top;
-            if(!o[k])o[k]=[];o[k].push({s:$t,h:$t.outerHeight()}); 
+            if(!o[k])o[k]=[];o[k].push({s:$t,h:$t.outerHeight()});
         })
         for(var k in o){
             var a=o[k].sort(function(a,b){return b.h-a.h}),l=a.length;
-            if(a[0].h!=a[l-1].h)for(var i=0;i<l;++i){ a[i].s.outerHeight(a[0].h); }
+            if(a[0].h!=a[l-1].h)for(var i=0;i<l;++i){ a[i].s.height(a[0].h);}
         }
     };
 })(jQuery);
