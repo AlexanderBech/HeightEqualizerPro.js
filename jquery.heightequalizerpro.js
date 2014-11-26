c22bbf44a996ff967044393f8f047331a5b93232
Alexander Bech <github@alexanderbech.com>
2014-11-26 17:01:49 +0100

    Updated to version 4

--- jquery.heightequalizerpro.js
+++ jquery.heightequalizerpro.js
@@ -1,18 +1,23 @@
 /* =========================================================
-*	HeightEqualizerPro.js 3.0
-*	Credits: Jacob Eriksen
+*	HeightEqualizerPro.js 4.0
+*	Credits: Alexander Bech
 *	http://github.com/AlexanderBech/HeightEqualizerPro.js
 * ========================================================== */
 (function($){
     $.fn.heightEqualizerPro=function(){
-    	var subItem=arguments[0]||null,o={};
+    	var subItem = arguments[0] || null,
+            greatestHeight = 0;
+
         this.each(function(){
-            var $t=subItem?$(subItem,$(this)):$(this);$t.height('auto');var k='_'+$t.offset().top;
-            if(!o[k])o[k]=[];o[k].push({s:$t,h:$t.outerHeight()});
-        })
-        for(var k in o){
-            var a=o[k].sort(function(a,b){return b.h-a.h}),l=a.length;
-            if(a[0].h!=a[l-1].h)for(var i=0;i<l;++i){ a[i].s.height(a[0].h);}
-        }
+            var $this = subItem ? $(subitem, $(this)) : $(this);
+            $this.height('auto');
+            var thisHeight = $this.height();
+            if(thisHeight>greatestHeight) greatestHeight = thisHeight;
+        });
+
+        this.each(function(){
+            var $this = subItem ? $(subitem, $(this)) : $(this);
+            $this.height(greatestHeight);
+        });
     };
 })(jQuery);
